1896db40bb731ced1f38b2c815bce27a4b1fe2c0
Pranav Veeraghanta <theveeraghanta@gmail.com>
2023-12-24 13:43:44 +0000

    12/24/2023

--- projects/music/music.js
+++ projects/music/music.js
@@ -156,7 +156,7 @@ audioElements.forEach(audio => {
     //on end to loop or not
 
     audio.addEventListener('ended', () => {
-
+        console.log(loopit)
         minuter = 0;
         secondr = 1;
 
