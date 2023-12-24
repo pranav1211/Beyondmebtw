edf5f2665e0041340d0d4a83edf6a727fd08b982
Pranav Veeraghanta <theveeraghanta@gmail.com>
2023-12-24 13:37:49 +0000

    12/24/2023

--- projects/music/music.js
+++ projects/music/music.js
@@ -65,7 +65,6 @@ audioElements.forEach(audio => {
             mcplay.style.visibility = 'visible';
             mcpause.style.visibility = 'hidden';
             totalchecker = '0';
-            
         });
 
         // music control play button
