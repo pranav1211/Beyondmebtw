48d4e17edac0dd39e46d2576f215e1ab6ebe7608
Pranav Veeraghanta <theveeraghanta@gmail.com>
2023-12-26 18:05:10 +0000

    12/26/2023

--- projects/music/music.js
+++ projects/music/music.js
@@ -85,6 +85,7 @@ audioElements.forEach(audio => {
             secondr = 1;
             mindiv.innerHTML = "0:";
             audio.currentTime = 0;
+            totalchecker = '0';
 
         }
 
@@ -125,6 +126,7 @@ audioElements.forEach(audio => {
 
     // next song button
     nextsong.addEventListener('click', () => {
+        
         if (noofsongs < newsongnumber) { // if the number of songs from json file is less than number that is used to get song id it means the end of playlist has been reached
             if (loopit == 'whole') {
                 songid = "#t1"
