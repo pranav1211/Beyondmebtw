904083e42ef2ba84644e4eef4e9213aa72e92ebc
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-01-02 14:32:48 +0000

    1/2/2024

--- projects/music/music.js
+++ projects/music/music.js
@@ -217,7 +217,7 @@ audioElements.forEach(audio => {
 
 /////////////////////////////////////////////////////////////////
 var p1 = document.getElementById("t1")
-var musicbars = document.querySelectorAll('.thebars')
+var musicbars = document.querySelector('.animationaud')
 document.querySelector('.showerbeckyg').addEventListener("click", () => {
     p1.play();
     p1.currentTime = 0;
