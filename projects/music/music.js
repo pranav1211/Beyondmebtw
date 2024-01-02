9ba35cd69d1d8e8108f03db11e028614fa7f6696
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-01-02 14:39:22 +0000

    1/2/2024

--- projects/music/music.js
+++ projects/music/music.js
@@ -230,7 +230,7 @@ document.querySelector('.showerbeckyg').addEventListener("click", () => {
     minuter = 0;
     secondr = 1;  
     musicbars.style.animation = 'bounce 4s ease infinite'
-    bar1.style.marginLeft = '0.6%'
+    bar1.style.marginLeft = '1.2%'
 });
 //////////////////////////////////////////////////////
 var p2 = document.getElementById("t2")
