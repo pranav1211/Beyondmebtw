53388e6a1529e327a94bb9c29c47d2f1e2662096
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-01-09 18:26:42 +0000

    1/9/2024

--- projects/music/music.js
+++ projects/music/music.js
@@ -163,7 +163,7 @@ audioElements.forEach(audio => {
     prevsong.addEventListener('click', () => {
         clearInterval(intervalid1);
         if (prevsongnumber == 0) { // checking if there exists a song before the current song
-            audio.currentTime = 3000;
+            audio.currentTime = 30000;
             mindiv.innerHTML = "0:";
             secdiv.innerHTML = "00"
             minuter = 0;
