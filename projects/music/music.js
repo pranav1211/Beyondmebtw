65edf8eb34bcbd85ba1a651b62c1289163449a1a
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-01-09 19:19:58 +0000

    1/10/2024

--- projects/music/music.js
+++ projects/music/music.js
@@ -174,8 +174,7 @@ audioElements.forEach(audio => {
         }
         else if (secondr > 4) {
             audio.currentTime = 0;
-        }
-        secondr = 0;
+        }       
     })
 
     //replay checker
