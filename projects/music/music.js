1487438e741f1069729bd9e88a25eceebc34152a
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-01-09 18:00:48 +0000

    1/9/2024

--- projects/music/music.js
+++ projects/music/music.js
@@ -159,7 +159,7 @@ audioElements.forEach(audio => {
 
     prevsong.addEventListener('click', () => {
         if (secondr > 2) {
-            currentlyPlaying.currentTime = 0;
+            audio.currentTime = 0;
             minuter = 0;
             secondr = 1;
         }
