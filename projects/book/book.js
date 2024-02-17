8b098ecf042e1f26cb8ec3913ca49091a38537e1
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-02-17 15:35:37 +0530

    17/2

--- projects/book/book.js
+++ projects/book/book.js
@@ -46,9 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
         img.id = ('qrimage')
         img.src = canvas.toDataURL()
         document.body.appendChild(img)
-    })
 
-    getcode.addEventListener('click', () => {
         imageurl = document.querySelector('#qrimage')
         urls = imageurl.getAttribute('src')
         const imageURL = urls
