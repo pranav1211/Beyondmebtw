a3147c1f127556488518bc19875047290285d250
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-05-08 00:55:28 +0530

    8/5/24

---
+++ nodetest/nodeserver.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+const port = 6123;
+
+app.get('/', (req, res) => {
+    fetch('http://64.227.143.61:6123', {
+        method: 'POST',
+        body: JSON.stringify({ /* Your POST data */ }),
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then(response => {
+        console.log('POST request sent');
+        res.sendStatus(200);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        res.sendStatus(500);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Node.js server listening at http://localhost:${port}`);
+});
