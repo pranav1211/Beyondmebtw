d7c9aaa1b0c6414650f5c024f37a30b4abd4800b
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-05-08 19:47:39 +0530

    8/5/24

---
+++ nodetest/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('A client connected');
+
+  socket.on('messageFromClient', (data) => {
+    console.log('Message from client:', data);
+    // Perform actions based on the received message
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
