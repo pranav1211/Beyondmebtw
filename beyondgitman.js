70e1bde8dce45fdcd4c71cc7a3f4f25c43ead03c
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-12 17:09:03 +0530

    12/9

--- beyondgitman.js
+++ beyondgitman.js
@@ -45,6 +45,6 @@ app.post('/bmbg', (req, res) => {
     });
 });
 
-app.listen(6005, () => {
-    console.log('Server is running on port 6005');
+app.listen(6007, () => {
+    console.log('Server is running on port 6007');
 });
