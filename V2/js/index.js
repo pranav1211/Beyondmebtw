c709a20e7d4814cd3c7462ae9dd9982e8df794b6
pranavisda1 <pranavisda1@gmail.com>
2025-01-14 23:30:24 +0530

    14/1

---
+++ V2/js/index.js
@@ -0,0 +1,6 @@
+home = document.querySelector("#home")
+
+home.style.fontWeight = "bold";
+home.style.textDecoration = 'underline'; // Ensures the underline is applied
+home.style.textUnderlineOffset = '13px'; // Adds offset to the underline
+
