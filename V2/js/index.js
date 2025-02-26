f9d6fce3140acef054718e62c73bb29ae09f4369
pranavisda1 <pranavisda1@gmail.com>
2025-02-27 01:10:43 +0530

    27/2

--- V2/js/index.js
+++
@@ -1,6 +0,0 @@
-home = document.querySelector("#home")
-
-home.style.fontWeight = "bold";
-home.style.textDecoration = 'underline'; // Ensures the underline is applied
-home.style.textUnderlineOffset = '13px'; // Adds offset to the underline
-
