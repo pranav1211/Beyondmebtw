d85c0f27e1db26c84c6426e3c58010439d1cdc61
pranavisda1 <pranavisda1@gmail.com>
2025-03-11 00:00:41 +0530

    11/3

--- manage/index.js
+++ manage/index.js
@@ -5,18 +5,27 @@ document.addEventListener("DOMContentLoaded", () => {
         form.addEventListener("submit", (event) => {
             event.preventDefault();
 
+            const formId = form.id;
             const formData = new FormData(form);
             const name = formData.get("name");
             const dateInput = formData.get("date");
             const excerpt = formData.get("excerpt");
             const thumbnail = formData.get("thumbnail");
+            const link = formData.get("link")
             const key = formData.get("key");
 
             const dateObject = new Date(dateInput);
             const options = { year: 'numeric', month: 'long', day: 'numeric' };
             const formattedDate = dateObject.toLocaleDateString('en-US', options);
 
-            const url = `https://manage.beyondmebtw.com/latestdata?name=${encodeURIComponent(name)}&date=${encodeURIComponent(formattedDate)}&excerpt=${encodeURIComponent(excerpt)}&thumbnail=${encodeURIComponent(thumbnail)}&key=${encodeURIComponent(key)}`;
+            const url = `https://manage.beyondmebtw.com/latestdata?
+            name=${encodeURIComponent(name)}
+            &date=${encodeURIComponent(formattedDate)}
+            &excerpt=${encodeURIComponent(excerpt)}
+            &thumbnail=${encodeURIComponent(thumbnail)}
+            &link=${encodeURIComponent(link)}
+            &formid=${encodeURIComponent(formId)}
+            &key=${encodeURIComponent(key)}`;
 
             fetch(url)
                 .then((response) => {
