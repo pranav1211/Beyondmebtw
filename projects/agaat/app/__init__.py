97f5ac0df81d35f857452e51ff59b6ad671721b6
pranavisda1 <pranavisda1@gmail.com>
2025-06-03 08:42:13 +0530

    03-06-2025

--- projects/agaat/app/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/__init__.py
-from flask import Flask
-from dotenv import load_dotenv
-
-def create_app():
-    app = Flask(__name__)    
-    load_dotenv()
-    app.secret_key = os.getenv('SECRET_KEY')
-    return app
