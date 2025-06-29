3edf7f330805b1bda6385027651d757850f25447
pranavisda1 <pranavisda1@gmail.com>
2025-06-29 17:23:40 +0530

    29-06-2025

--- beyondgitman.js
+++ beyondgitman.js
@@ -1,32 +1,9 @@
 const express = require('express');
-const crypto = require('crypto');
 const { exec } = require('child_process');
-const path = require('path');
-require('dotenv').config();
 
 const scriptPath = '/shellfiles/beyond.sh';
-const GITHUB_SECRET = process.env.beyondmegitkey;
 
 const app = express();
-
-app.use(express.json());
-app.use((req, res, next) => {
-    const signature = req.headers['x-hub-signature-256'];
-    const payload = JSON.stringify(req.body);
-
-    if (!signature) {
-        return res.status(401).json({ message: 'Signature required' });
-    }
-
-    const hmac = crypto.createHmac('sha256', GITHUB_SECRET);
-    const digest = `sha256=${hmac.update(payload).digest('hex')}`;
-
-    if (signature !== digest) {
-        return res.status(401).json({ message: 'Invalid signature' });
-    }
-
-    next();
-});
 
 app.post('/bmbg', (req, res) => {
     exec(`sh ${scriptPath}`, (error, stdout, stderr) => {
