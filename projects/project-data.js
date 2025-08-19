702c1adf07b281309a6ec368fee794895bd553d3
pranavisda1 <pranavisda1@gmail.com>
2025-08-19 18:40:45 +0530

    19-08-2025

--- projects/project-data.js
+++ projects/project-data.js
@@ -81,6 +81,18 @@ const projectsData = [
     githubLink: "https://github.com/pranav1211/Uncharted-3",
     category: "Game Development",
     tags: ["Next.js", "HTML", "CSS", "Vercel"]
+  },
+  {
+    id: 8,
+    title: "F1 Haptic Trailer: Android Recreation",
+    shortDescription: "A haptic feedback experience for F1 fans",
+    fullDescription: "• A digital recreation of the F1 haptic trailer, optimized for Android devices. \n • Features immersive haptic feedback synchronized with video content. \n • Developed using web technologies to ensure broad compatibility.",
+    logo: "https://beyondmebtw.com/assets/images/favicon.ico",
+    images: ["/assets/images/projects/f1haptic.webp"],
+    link: "https://beyondmebtw.com/projects/f1haptic",
+    githubLink: "https://github.com/pranav1211/Uncharted-3",
+    category: "Game Development",
+    tags: ["Next.js", "HTML", "CSS", "Vercel"]
   }
 
 ];
